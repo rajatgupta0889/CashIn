fc084652db6a6bdfd6a45197e684a4f554b72432
pravk <hello.praveen@gmail.com>
2015-06-26 10:25:38 +0530

    Login form Validation

--- app/src/main/java/com/mantralabsglobal/cashin/LoginActivity.java
+++ app/src/main/java/com/mantralabsglobal/cashin/LoginActivity.java
@@ -6,12 +6,20 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.mantralabsglobal.cashin.Activity.BaseActivity;
 import com.mantralabsglobal.cashin.rest.RestClient;
 import com.mantralabsglobal.cashin.service.AuthenticationService;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
+
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -21,25 +29,37 @@ import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements Validator.ValidationListener {
 
+    @NotEmpty
+    @Email
     @InjectView(R.id.et_userName)
     EditText et_userName;
 
+    @Password
     @InjectView(R.id.et_password)
     EditText et_password;
+
+    Validator validator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.inject(this);
+        validator = new Validator(this);
+        validator.setValidationListener(this);
     }
 
 
     @OnClick(R.id.loginButton)
     public void onLogin()
     {
+        validator.validate();
+    }
+
+    @Override
+    public void onValidationSucceeded() {
         showProgressDialog( getString(R.string.title_please_wait),getString(R.string.authenticating),true, false);
         RestClient restClient =((Application) getApplication()).getRestClient();
 
@@ -77,4 +97,18 @@ public class LoginActivity extends BaseActivity {
         });
     }
 
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
